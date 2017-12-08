dfd6b0efe7c908cf54421ba6b03b9ac332ee6c81
Bended <benoit.deddouch@gmail.com>
2017-12-08 23:00:33 +0200

    Initial version

--- test_perso.js
+++ test_perso.js
@@ -1,41 +1,73 @@
 function next1() {
         document.getElementById("q1").style.display = "none";
         document.getElementById("q2").style.display = "block";
+        document.getElementById("step1").style.background = "white";
+        document.getElementById("step1").style.color = "black";
+        document.getElementById("step2").style.background = "#008CBA";
+        document.getElementById("step2").style.color = "white";
     }
-function preview1() {
-}
 
 function next2() {
         document.getElementById("q2").style.display = "none";
         document.getElementById("q3").style.display = "block";
+        document.getElementById("step2").style.background = "white";
+        document.getElementById("step2").style.color = "black";
+        document.getElementById("step3").style.background = "#008CBA";
+        document.getElementById("step3").style.color = "white";
     }
 
 function next3() {
         document.getElementById("q3").style.display = "none";
         document.getElementById("q4").style.display = "block";
+        document.getElementById("step3").style.background = "white";
+        document.getElementById("step3").style.color = "black";
+        document.getElementById("step4").style.background = "#008CBA";
+        document.getElementById("step4").style.color = "white";
     }
 
 function next4() {
         document.getElementById("q4").style.display = "none";
         document.getElementById("q5").style.display = "block";
+        document.getElementById("step4").style.background = "white";
+        document.getElementById("step4").style.color = "black";
+        document.getElementById("step5").style.background = "#008CBA";
+        document.getElementById("step5").style.color = "white";
     }
 
 function prev2() {
         document.getElementById("q2").style.display = "none";
         document.getElementById("q1").style.display = "block";
+        document.getElementById("step2").style.background = "white";
+        document.getElementById("step2").style.color = "black";
+        document.getElementById("step1").style.background = "#008CBA";
+        document.getElementById("step1").style.color = "white";
     }
 
 function prev3() {
         document.getElementById("q3").style.display = "none";
         document.getElementById("q2").style.display = "block";
+        document.getElementById("step3").style.background = "white";
+        document.getElementById("step3").style.color = "black";
+        document.getElementById("step2").style.background = "#008CBA";
+        document.getElementById("step2").style.color = "white";
     }
 
 function prev4() {
         document.getElementById("q4").style.display = "none";
         document.getElementById("q3").style.display = "block";
+        document.getElementById("step4").style.background = "white";
+        document.getElementById("step4").style.color = "black";
+        document.getElementById("step3").style.background = "#008CBA";
+        document.getElementById("step3").style.color = "white";
     }
 
 function prev5() {
         document.getElementById("q5").style.display = "none";
         document.getElementById("q4").style.display = "block";
+        document.getElementById("step5").style.background = "white";
+        document.getElementById("step5").style.color = "black";
+        document.getElementById("step4").style.background = "#008CBA";
+        document.getElementById("step4").style.color = "white";
     }
+
+
