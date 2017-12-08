aecacfbb66bdb5fb8693f934abbc94a8797425ad
Bended <benoit.deddouch@gmail.com>
2017-12-08 16:43:13 +0200

    Initial version

---
+++ test_perso.js
@@ -0,0 +1,41 @@
+function next1() {
+        document.getElementById("q1").style.display = "none";
+        document.getElementById("q2").style.display = "block";
+    }
+function preview1() {
+}
+
+function next2() {
+        document.getElementById("q2").style.display = "none";
+        document.getElementById("q3").style.display = "block";
+    }
+
+function next3() {
+        document.getElementById("q3").style.display = "none";
+        document.getElementById("q4").style.display = "block";
+    }
+
+function next4() {
+        document.getElementById("q4").style.display = "none";
+        document.getElementById("q5").style.display = "block";
+    }
+
+function prev2() {
+        document.getElementById("q2").style.display = "none";
+        document.getElementById("q1").style.display = "block";
+    }
+
+function prev3() {
+        document.getElementById("q3").style.display = "none";
+        document.getElementById("q2").style.display = "block";
+    }
+
+function prev4() {
+        document.getElementById("q4").style.display = "none";
+        document.getElementById("q3").style.display = "block";
+    }
+
+function prev5() {
+        document.getElementById("q5").style.display = "none";
+        document.getElementById("q4").style.display = "block";
+    }
